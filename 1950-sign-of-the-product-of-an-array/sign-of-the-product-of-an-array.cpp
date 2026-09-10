e5f0f461a190e5b8e967cbab4f81e80e4c4c5e5d
Nitin sharma <sharmanitin6301@gmail.com>
2026-09-10 22:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (11.15%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count = 1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+            return 0;
+            if(nums[i]>0)
+            count = count*1;
+            else
+            count = count*(-1);
+        }
+        return count ;
+    }
+};
